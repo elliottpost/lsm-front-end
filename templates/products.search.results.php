7f3dbff0ff92943ba3de7988c743aae66296b0ab
Ellytronic <elliott@ellytronic.com>
2015-11-25 22:04:48 -0600

    started integrating curl class

--- templates/products.search.results.php
+++ templates/products.search.results.php
@@ -5,59 +5,49 @@ if( !isset( $_REQUEST['q'] ) || empty( $_REQUEST['q'] ) ) {
     return;
 }
 
-require_once 'head.php';
+$lsm = new LsmCurl;
+$lsm->setEndpoint( LSM_API_ENDPOINT . "products" . DS . $_REQUEST['q'] );
+$lsm->useGet();
+$lsm->sendRequest();
+
+Util::getHeader();
 ?>
 
-        <!-- Page Heading -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">Products
-                    <small>Showing products matching: <em><?=$_REQUEST['q']?></em></small>
-                </h1>
-            </div>
+    <!-- Page Heading -->
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Products
+                <small>Showing products matching: <em><?=$_REQUEST['q']?></em></small>
+            </h1>
         </div>
-        <!-- /.row -->
+    </div>
+    <!-- /.row -->
 
-        <!-- Project One -->
-        <?php
-        $ch = curl_init();        
-        $url = "http://lsm1.herokuapp.com/services/lsm/products/es";
-        curl_setopt( $ch, CURLOPT_URL, $url );
-        $headers = array();
-        $headers[] = 'Accept: application/json';
-        $headers[] = 'Content-Type: application/json';
-
-        curl_setopt( $ch, CURLOPT_HTTPHEADER, $headers );
-        curl_setopt( $ch, CURLOPT_HTTPGET, true );
-        ob_start();
-        curl_exec( $ch );
-        $products = json_decode( ob_get_clean() );
-
-        //close connection
-        curl_close( $ch );
-        // echo "<pre>";var_dump( $products );die();
-        if( $products ) {
-            foreach( $products as $product ) {
-                ?>
-                <div class="row">
-                    <div class="col-md-7">
-                        <a href="#">
-                            <img class="img-responsive" src="http://placehold.it/700x300" alt="">
-                        </a>
-                    </div>
-                    <div class="col-md-5">
-                        <h3><?=$product->productName;?></h3>
-                        <h4>$<?=money_format( '%i', $product->price );?></h4>
-                        <p><?=$product->description?></p>
-                        <a class="btn btn-primary" href="#">Buy Product <span class="glyphicon glyphicon-chevron-right"></span></a>
-                    </div>
+    <!-- Project One -->
+    <?php
+    $products = json_decode( $lsm->getResponseContent() );
+    if( $products ) {
+        foreach( $products as $product ) {
+            ?>
+            <div class="row">
+                <div class="col-md-7">
+                    <a href="#">
+                        <img class="img-responsive" src="http://placehold.it/700x300" alt="">
+                    </a>
                 </div>
-                <!-- /.row -->
+                <div class="col-md-5">
+                    <h3><?=$product->productName;?></h3>
+                    <h4>$<?=money_format( '%i', $product->price );?></h4>
+                    <p><?=$product->description?></p>
+                    <a class="btn btn-primary" href="#">Buy Product <span class="glyphicon glyphicon-chevron-right"></span></a>
+                </div>
+            </div>
+            <!-- /.row -->
 
-                <hr>
+            <hr>
 
-                <?php
-            } //foreach products as product
-        } //has products
- 
-require_once 'foot.php';
+            <?php
+        } //foreach products as product
+    } //has products
+
+Util::getFooter();
