a3a27b4ac59ab8f760cdf141458a9a836656fddb
Elly Post <elly@ellytronic.media>
2015-11-30 19:07:13 -0600

    Home page form now link to correct page

--- templates/index.php
+++ templates/index.php
@@ -5,7 +5,7 @@ Util::getHeader();
 <div class="row">
     <div class="col-lg-12">
         <h1 class="page-header">Search for products</h1>
-        <form role="search" action='search-products.php' method='post'>    
+        <form role="search" action='<?=SITE_URI?>products/search/results' method='post'>    
         <div class="input-group">
             <input type="text" class="form-control" placeholder="Search Products" name="q">
             <div class="input-group-btn">
